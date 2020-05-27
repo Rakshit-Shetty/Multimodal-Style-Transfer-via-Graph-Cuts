1e77cf25201bef2fecfc06f944713665d30a7d02
Rakshit Shetty <callrmshetty@gmail.com>
2020-05-27 17:07:01 +0530

    resolving typos and bugs

--- VGGNormalized.py
+++ VGGNormalized.py
@@ -1,8 +1,7 @@
 import torch
 import torch.nn as nn
 
-VGG_NORMALIZED_RELU5_1 = nn.Sequential(
-
+normalised_vgg_relu5_1 = nn.Sequential(
 	nn.Conv2d(3, 3, 1),
 	nn.ReflectionPad2d((1, 1, 1, 1)),
 	nn.Conv2d(3, 64, 3),
@@ -49,13 +48,62 @@ VGG_NORMALIZED_RELU5_1 = nn.Sequential(
 	nn.ReLU()
 	)
 
-class  VGGNormalized(nn.Module):
-	"""docstring for  VGGNormalized"""
-	def __init__(self, pretrained_path='vgg_normalized_conv5_1.pth'):
+
+class NormalisedVGG(nn.Module):
+	"""
+	VGG reluX_1(X = 1, 2, 3, 4, 5) can be obtained by slicing the follow vgg5_1 model.
+	Sequential(
+	(0): Conv2d(3, 3, kernel_size=(1, 1), stride=(1, 1))
+	(1): ReflectionPad2d((1, 1, 1, 1))
+	(2): Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1))
+	(3): ReLU() # relu1_1
+	(4): ReflectionPad2d((1, 1, 1, 1))
+	(5): Conv2d(64, 64, kernel_size=(3, 3), stride=(1, 1))
+	(6): ReLU()
+	(7): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=True)
+	(8): ReflectionPad2d((1, 1, 1, 1))
+	(9): Conv2d(64, 128, kernel_size=(3, 3), stride=(1, 1))
+	(10): ReLU() # relu2_1
+	(11): ReflectionPad2d((1, 1, 1, 1))
+	(12): Conv2d(128, 128, kernel_size=(3, 3), stride=(1, 1))
+	(13): ReLU()
+	(14): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=True)
+	(15): ReflectionPad2d((1, 1, 1, 1))
+	(16): Conv2d(128, 256, kernel_size=(3, 3), stride=(1, 1))
+	(17): ReLU() # relu3_1
+	(18): ReflectionPad2d((1, 1, 1, 1))
+	(19): Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1))
+	(20): ReLU()
+	(21): ReflectionPad2d((1, 1, 1, 1))
+	(22): Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1))
+	(23): ReLU()
+	(24): ReflectionPad2d((1, 1, 1, 1))
+	(25): Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1))
+	(26): ReLU()
+	(27): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=True)
+	(28): ReflectionPad2d((1, 1, 1, 1))
+	(29): Conv2d(256, 512, kernel_size=(3, 3), stride=(1, 1))
+	(30): ReLU()# relu4_1
+	(31): ReflectionPad2d((1, 1, 1, 1))
+	(32): Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1))
+	(33): ReLU()
+	(34): ReflectionPad2d((1, 1, 1, 1))
+	(35): Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1))
+	(36): ReLU()
+	(37): ReflectionPad2d((1, 1, 1, 1))
+	(38): Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1))
+	(39): ReLU()
+	(40): MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=True)
+	(41): ReflectionPad2d((1, 1, 1, 1))
+	(42): Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1))
+	(43): ReLU() # relu5_1
+	)
+	"""
+	def __init__(self, pretrained_path='vgg_normalised_conv5_1.pth'):
 		super().__init__()
-		self.net = VGG_NORMALIZED_RELU5_1
+		self.net = normalised_vgg_relu5_1
 		if pretrained_path is not None:
-			self.net.load_state_dict(torch.load(pretrained_path, map_location=lambda storage, loc:storage))
+			self.net.load_state_dict(torch.load(pretrained_path, map_location=lambda storage, loc: storage))
 
 	def forward(self, x, target, output_last_feature=False):
 		if target == 'relu1_1':
@@ -69,5 +117,4 @@ class  VGGNormalized(nn.Module):
 		elif target == 'relu5_1':
 			return self.net(x)
 		else:
-			raise ValueEroor(f'target should be one of relu1_1 or relu2_1 or relu3_1 or relu4_1 or relu5_1 but not {target}')
-
+			raise ValueError(f'target should be in ["relu1_1", "relu2_1", "relu3_1", "relu4_1", "relu5_1"] but not {target}')
